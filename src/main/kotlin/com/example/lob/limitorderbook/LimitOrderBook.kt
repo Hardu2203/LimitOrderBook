f159d3a2693fd1b6b693b603f90e954f86a12a93
hardunel <hardunelhn@gmail.com>
2024-02-05 11:09:55 +0200

    Switched to a treemap implementation because of the get orderbook endpoint

--- src/main/kotlin/com/example/lob/limitorderbook/LimitOrderBook.kt
+++ src/main/kotlin/com/example/lob/limitorderbook/LimitOrderBook.kt
@@ -13,8 +13,8 @@ import kotlin.math.min
 
 @Component
 class LimitOrderBook(
-    private val bestAsk: PriorityQueue<LimitPriceOrders> = PriorityQueue { o1, o2 -> o1.price.compareTo(o2.price) },
-    private val bestBid: PriorityQueue<LimitPriceOrders> = PriorityQueue { o1, o2 -> o2.price.compareTo(o1.price) },
+    private val bestAsk: TreeMap<BigDecimal, LimitPriceOrders> = TreeMap(),
+    private val bestBid: TreeMap<BigDecimal, LimitPriceOrders> = TreeMap(Collections.reverseOrder()),
     private val orderMap: MutableMap<OrderId, Order> = mutableMapOf(),
     private val volumeMap: MutableMap<Pair<BigDecimal, BuyOrSellEnum>, BigDecimal> = mutableMapOf(),
     private val orderQueueMap: MutableMap<Pair<BigDecimal, BuyOrSellEnum>, LimitPriceOrders> = mutableMapOf(),
@@ -24,13 +24,13 @@ class LimitOrderBook(
     fun addOrder(order: Order) {
         val (sameSide, otherSide, sidePriceComparison) = getTradeSideDetail(order)
 
-        while (order.volume > BigDecimal.ZERO && otherSide.isNotEmpty() && sidePriceComparison(
-                otherSide.peek().orders.peek().price,
+        while (order.volume > BigDecimal.ZERO && checkOtherSideBestPriceIsNotEmpty(otherSide) && sidePriceComparison(
+                getBestPrice(otherSide),
                 order.price
             )
         ) {
-            val otherLimitPriceOrders = otherSide.peek()
-            val otherOrder = otherLimitPriceOrders.orders.peek()
+            val otherLimitPriceOrders = getLimitPriceOrders(otherSide)
+            val otherOrder = getBestOrder(otherLimitPriceOrders)
             val tradePrice = otherOrder.price
             val tradeVolume = min(order.volume, otherOrder.volume)
             val tradeQuantity = min(order.quantity, otherOrder.quantity)
@@ -78,7 +78,7 @@ class LimitOrderBook(
             if (limitPriceOrders.orders.size == 0) {
                 orderQueueMap.remove(order.price to order.buyOrSellEnum)
                 val (sameSide) = getTradeSideDetail(order)
-                sameSide.remove(limitPriceOrders)
+                sameSide.remove(limitPriceOrders.price)
             }
             if (order.volume > BigDecimal.ZERO) reduceVolumeMap(order.price, order.buyOrSellEnum, order.volume)
             orderMap.remove(orderId)
@@ -86,11 +86,19 @@ class LimitOrderBook(
     }
 
     fun getBestBidOrNull(): Order? {
-        return bestBid.peek()?.orders?.peek()
+        return if (bestBid.isEmpty()) {
+            null // Return null if the TreeMap is empty
+        } else {
+            bestBid[bestBid.firstKey()]?.orders?.peek()
+        }
     }
 
     fun getBestAskOrNull(): Order? {
-        return bestAsk.peek()?.orders?.peek()
+        return if (bestAsk.isEmpty()) {
+            null
+        } else {
+            bestAsk[bestAsk.firstKey()]?.orders?.peek()
+        }
     }
 
     fun getOrderQueue(price: BigDecimal, buyOrSellEnum: BuyOrSellEnum): LimitPriceOrders? {
@@ -115,21 +123,46 @@ class LimitOrderBook(
     }
 
     fun <T> getBestBids(first: Int, mapping: (LimitPriceOrders) -> T): List<T> {
-        val minFirst = min(bestBid.size, first)
         val bestBidList: MutableList<T> = mutableListOf()
-        repeat(minFirst) {
-            bestBidList.add(bestBid.peek().let(mapping))
+
+        bestBid.entries.take(min(bestBid.size, first)).forEach { entry ->
+            entry.value.let { limitPriceOrders ->
+                bestBidList.add(mapping(limitPriceOrders))
+            }
         }
+
         return bestBidList
     }
-
     fun <T> getBestAsks(first: Int, mapping: (LimitPriceOrders) -> T): List<T> {
-        val minFirst = min(bestAsk.size, first)
         val bestAskList: MutableList<T> = mutableListOf()
-        repeat(minFirst) {
-            bestAskList.add(bestAsk.peek().let(mapping))
+
+        bestAsk.entries.take(min(bestAsk.size, first)).forEach { entry ->
+            entry.value.let { limitPriceOrders ->
+                bestAskList.add(mapping(limitPriceOrders))
+            }
         }
+
         return bestAskList
+    }
+
+    private fun getLimitPriceOrders(otherSide: TreeMap<BigDecimal, LimitPriceOrders>) =
+        otherSide[otherSide.firstKey()]
+            ?: throw IllegalStateException("Other limit order price order should not be null")
+
+    private fun getBestOrder(otherLimitPriceOrders: LimitPriceOrders?) =
+        otherLimitPriceOrders?.orders?.peek() ?: throw IllegalStateException("Best order should not be null")
+
+    private fun checkOtherSideBestPriceIsNotEmpty(otherSide: TreeMap<BigDecimal, LimitPriceOrders>): Boolean {
+        return if (otherSide.isEmpty()) {
+            false
+        } else {
+            val bestPrice = otherSide.firstKey()
+            otherSide[bestPrice]?.orders?.peek()?.price != null
+        }
+    }
+
+    private fun getBestPrice(otherSide: TreeMap<BigDecimal, LimitPriceOrders>): BigDecimal {
+        return otherSide[otherSide.firstKey()]?.orders?.peek()?.price ?: throw IllegalStateException("Best price should not be null")
     }
 
     private fun addToTradeHistory(trade: Trade) {
@@ -151,7 +184,7 @@ class LimitOrderBook(
 
     private fun addOrderToBook(
         order: Order,
-        sameSide: PriorityQueue<LimitPriceOrders>,
+        sameSide: TreeMap<BigDecimal, LimitPriceOrders>,
     ) {
         logger.info { "Adding to orderbook: orderId ${order.orderId}, side ${order.buyOrSellEnum}, for ${order.quantity} @ ${order.price} " }
         val queue = orderQueueMap[order.price to order.buyOrSellEnum]
@@ -165,7 +198,7 @@ class LimitOrderBook(
             val linkedList = LinkedList<Order>()
             linkedList.add(order)
             val limitPriceOrders = LimitPriceOrders(order.price, order.quantity, linkedList)
-            sameSide.add(limitPriceOrders)
+            sameSide[order.price] = (limitPriceOrders)
             orderQueueMap[order.price to order.buyOrSellEnum] = limitPriceOrders
             volumeMap[order.price to order.buyOrSellEnum] = order.volume
         }
